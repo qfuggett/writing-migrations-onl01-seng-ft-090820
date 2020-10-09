a8353af60ce293e30967692c61d07d9e00e04520
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-09 18:10:16 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,6 +2,7 @@ class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
   
   def change
     add_column :students do |t|
+      t.string :name
       t.integer :grade
       t.string :datetime
     end
