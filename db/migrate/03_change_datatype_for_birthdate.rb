0b2c01d980864c15160b824ac378fe0446e4d4a2
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-09 18:22:37 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,6 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
-  def change(table_name, column_name, type)
+  def change
     change_column :students, :datetime, :string
   end
 end 
